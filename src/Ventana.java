747505a496431e4106054bda3c1b6c947813bd81
Andrestabango <andres.tabango@udla.edu.ec>
2024-04-10 17:21:51 -0500

    Proyecto pilas

--- src/Ventana.java
+++ src/Ventana.java
@@ -19,6 +19,46 @@ public class Ventana {
             public void actionPerformed(ActionEvent e) {
                 pila.apilar(Integer.parseInt(textNumero.getText()));
                 textArea1.setText(pila.toString());
+                textNumero.setText("");
+            }
+        });
+        desapilarButton.addActionListener(new ActionListener() {
+            //llamar a una accion
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JOptionPane.showConfirmDialog(null,"El dato sacado es "+pila.desapilar());
+                    textArea1.setText(pila.toString());
+                } catch (Exception ex) {
+                    JOptionPane.showConfirmDialog(null,ex.getMessage());
+                }
+
+            }
+        });
+        cimaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JOptionPane.showConfirmDialog(null,"El dato sacado es "+pila.cima());
+                    textArea1.setText(pila.toString());
+                } catch (Exception ex) {
+                    JOptionPane.showConfirmDialog(null,ex.getMessage());
+                }
+            }
+        });
+        buscarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int indice = pila.buscarElemento(Integer.parseInt(textNumero.getText()));
+                   if(indice ==-1){
+                       JOptionPane.showConfirmDialog(null,"Elemento no encontrado");
+                   }else
+                       JOptionPane.showConfirmDialog(null,"El elemento "+ textNumero.getText()+ "Se encuentra en la posicion "+indice);
+                } catch (Exception ex) {
+                    JOptionPane.showConfirmDialog(null,ex.getMessage());
+                }
+
             }
         });
     }
