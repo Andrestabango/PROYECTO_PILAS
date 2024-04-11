dd76fbc773b053bdcf34d79e2f2e90a9fe37266b
Andrestabango <andres.tabango@udla.edu.ec>
2024-04-10 21:52:43 -0500

    Proyecto pilas

--- src/Transformacion.java
+++ src/Transformacion.java
@@ -1,6 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Transformacion {
     private JTextArea textArea1;
-    private JButton button1;
+    private JButton Calcular;
+    private JScrollBar scrollBar1;
+    private Pila pila = new Pila();
+
+
+    public Transformacion() {
+
+
+        Calcular.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+                    String mensaje = "";
+                    for (int i=.numero()-1; i>= 0; i--){
+                        mensaje += numeros.get(i)+ "\n";
+                    }
+                    return mensaje;
+                }
+
+
+
+            }
+        });
+    }
 }
